6bdbc989b45874b8089788b7049b364973a45c4c
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-08-30 16:36:29 +0200

    Create pronamic-pay-test-helper.php

---
+++ pronamic-pay-test-helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay Test Helper
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-test-helper/
+ * Description: This plugin makes testing Pronamic Pay easier.
+ *
+ * Version: 1.0.0
+ * Requires at least: 4.7
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-test-helper
+ * Domain Path: /languages/
+ *
+ * License: GPL-3.0-or-later
+ *
+ * Depends: wp-pay/core
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-test-helper
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ */
+
